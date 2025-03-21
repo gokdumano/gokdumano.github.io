80e808e4df51340cf442928ed24dd9890d16a075
Deniz G. <53919703+gokdumano@users.noreply.github.com>
2025-03-21 16:44:27 +0300

    Add files via upload

--- metro-istanbul/helper.js
+++ metro-istanbul/helper.js
@@ -1,43 +1,58 @@
-async function CheckDB(dbname='MetroIstanbulDB', dbversion=1) {
-	const dbs = await window.indexedDB.databases();
-	for (let db of dbs){
-		if (db.name == dbname & db.version == dbversion){
-			return true;
-		}
-	}
-	return false;
+async function CheckDB(dbname = 'MetroIstanbulDB', dbversion = 1) {
+    if (!window.indexedDB) {
+        return false;
+    }
+    let dbs = await window.indexedDB.databases();
+    for (let db of dbs) {
+        if (db.name == dbname && db.version == dbversion) {
+            return true;
+        }
+    }
+    return false;
 }
-
-async function CheckTable(relname, dbname='MetroIstanbulDB', dbversion=1) {
-	if ( !await CheckDB(dbname, dbversion) ) {
-		return false;
-	}
-	
-	const DBOpenRequest = window.indexedDB.open(dbname, dbversion);
-	// https://stackoverflow.com/questions/67547038/how-do-i-check-if-an-indexeddb-database-already-exists
-	// https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory/databases#examples
-	
-	
+async function CheckObjStore(objStoreName, dbname = 'MetroIstanbulDB', dbversion = 1) {
+    return new Promise((resolve, reject) => {
+        if (!CheckDB(dbname, dbversion)) {
+            reject(new Error('CheckDB function returned with _false_ value'));
+        }
+        const OpenDBRequest = indexedDB.open(dbname, dbversion);
+        OpenDBRequest.onsuccess = (event) => {
+            const db = event.target.result;
+            const objectStoreExists = db.objectStoreNames.contains(objStoreName);
+            db.close();
+            resolve(objectStoreExists);
+        };
+        OpenDBRequest.onerror = (event) => {
+            db.close();
+            reject(new Error(event.target.error));
+        };
+    });
 }
-
-let CheckDB = async (dbname = 'MetroIstanbulDB') => await window.indexedDB.databases().then(dbs => dbs.some(db => db.name === dbname));
-
-await window.indexedDB.databases().then((dbs) => dbs.forEach(db => {if (db.name === dbname) {return db}}))
-
-
-async function CheckTable(relname, dbname = '') {
-	CheckDB(dbname).then(dbexists => {
-		if (dbexists) {
-			console.log(`Found ${dbname}`);
-		}
-		else {
-			throw new Error(`No IndexedDB found named ${dbname}`);
-		}
-	});
+async function IsObjStoreEmpty(objStoreName, dbname = 'MetroIstanbulDB', dbversion = 1) {
+    return new Promise((resolve, reject) => {
+        if (!CheckObjStore(dbname, dbversion)) {
+            reject(new Error('CheckObjStore function returned with _false_ value'));
+        }
+        // IndexedDB'yi aç
+        const OpenDBRequest = indexedDB.open(dbname, dbversion);
+        OpenDBRequest.onsuccess = (event) => {
+            const db = event.target.result;
+            // Transaction oluştur ve object store'a eriş
+            const transaction = db.transaction(objStoreName, 'readonly');
+            const objectStore = transaction.objectStore(objStoreName);
+            // Object store'daki kayıt sayısını al
+            const countRequest = objectStore.count();
+            countRequest.onsuccess = () => {
+                db.close();
+                resolve(countRequest.result === 0);
+            };
+            countRequest.onerror = () => {
+                db.close();
+                reject(new Error(event.target.error));
+            };
+        };
+        OpenDBRequest.onerror = (event) => {
+            reject(new Error("Failed to open the database."));
+        };
+    });
 }
-
-async function InitDB(dbname = 'MetroIstanbulDB') {
-	
-}
-
-
